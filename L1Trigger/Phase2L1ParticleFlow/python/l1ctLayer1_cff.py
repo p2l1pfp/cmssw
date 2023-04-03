49e6d17bca9c5658e776ad542942383e2d87e3db
Gianluca <gianluca.cerminara@cern.ch>
2023-04-03 11:34:18 +0200

    Update tight WP to use 90%eff score.

--- L1Trigger/Phase2L1ParticleFlow/python/l1ctLayer1_cff.py
+++ L1Trigger/Phase2L1ParticleFlow/python/l1ctLayer1_cff.py
@@ -294,6 +294,12 @@ l1tLayer1HGCal = cms.EDProducer("L1TCorrelatorLayer1Producer",
     writeRawHgcalCluster = cms.untracked.bool(True)
 )
 
+l1tLayer1HGCalElliptic = l1tLayer1HGCal.clone(
+    tkEgAlgoParameters = l1tLayer1HGCal.tkEgAlgoParameters.clone(
+        doCompositeTkEle = False,
+        trkQualityPtMin = 10.)
+)
+
 l1tLayer1HGCalExtended = l1tLayer1HGCal.clone(tracks = cms.InputTag('l1tPFTracksFromL1TracksExtended'))
 
 l1tLayer1HGCalNoTK = cms.EDProducer("L1TCorrelatorLayer1Producer",
@@ -530,6 +536,50 @@ l1tLayer1EG = cms.EDProducer(
     )
 )
 
+l1tLayer1EGElliptic = cms.EDProducer(
+    "L1TEGMultiMerger",
+    tkElectrons=cms.VPSet(
+        cms.PSet(
+            instance=cms.string("L1TkEleEE"),
+            pfProducers=cms.VInputTag(
+                cms.InputTag("l1tLayer1HGCalElliptic", 'L1TkEle')
+            )
+        ),
+        cms.PSet(
+            instance=cms.string("L1TkEleEB"),
+            pfProducers=cms.VInputTag(
+                cms.InputTag("l1tLayer1Barrel", 'L1TkEle')
+            )
+        )
+    ),
+    tkEms=cms.VPSet(
+        cms.PSet(
+            instance=cms.string("L1TkEmEE"),
+            pfProducers=cms.VInputTag(
+                cms.InputTag("l1tLayer1HGCalElliptic", 'L1TkEm'),
+                cms.InputTag("l1tLayer1HGCalNoTK", 'L1TkEm')
+            )
+        ),
+        cms.PSet(
+            instance=cms.string("L1TkEmEB"),
+            pfProducers=cms.VInputTag(
+                cms.InputTag("l1tLayer1Barrel", 'L1TkEm')
+            )
+        )
+    ),
+    tkEgs=cms.VPSet(
+        cms.PSet(
+            instance=cms.string("L1EgEE"),
+            pfProducers=cms.VInputTag(
+                cms.InputTag("l1tLayer1HGCalElliptic", 'L1Eg'),
+                cms.InputTag("l1tLayer1HGCalNoTK", 'L1Eg')
+            )
+        )    
+    )
+)
+
+
+
 L1TLayer1TaskInputsTask = cms.Task(
     l1tPFClustersFromL1EGClusters,
     l1tPFClustersFromCombinedCaloHCal,
@@ -546,5 +596,7 @@ L1TLayer1Task = cms.Task(
      l1tLayer1HF,
      l1tLayer1,
      l1tLayer1Extended,
-     l1tLayer1EG
+     l1tLayer1HGCalElliptic,
+     l1tLayer1EG,
+     l1tLayer1EGElliptic
 )
