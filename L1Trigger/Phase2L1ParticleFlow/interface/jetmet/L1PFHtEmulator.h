a38aa836abbd950679b6c4f0783f9f474dbc789b
Emyr Clement <emyr.clement@bristol.ac.uk>
2026-07-06 18:08:23 +0000

    Refine cordic calculation to get closer to hls implementation, code-checks/format.

--- L1Trigger/Phase2L1ParticleFlow/interface/jetmet/L1PFHtEmulator.h
+++ L1Trigger/Phase2L1ParticleFlow/interface/jetmet/L1PFHtEmulator.h
@@ -28,6 +28,42 @@ namespace P2L1HTMHTEmu {
 
   static constexpr int N_TABLE = 2048;
 
+  // Constants for CORDIC atan2 implementation
+  // Based on those used by hls implementation
+  static constexpr int CORDIC_INPUT_W = pxy_t::width;
+  static constexpr int CORDIC_INPUT_I = pxy_t::iwidth;
+  static constexpr int CORDIC_GUARD_BITS = 7;
+  static constexpr int CORDIC_WORKING_W =
+      CORDIC_INPUT_W + CORDIC_GUARD_BITS;                // Larger internal working bitwidth with "enough" guard bits
+  static constexpr int CORDIC_WORKING_I = 3;             // Number of integer bits in CORDIC working representation
+  static constexpr int CORDIC_NITER = CORDIC_WORKING_W;  // Number of iterations to perform
+
+  // Fixed constants, in precision used by the HLS implementation and their exact values
+  static const ap_fixed<CORDIC_INPUT_W + 1, CORDIC_WORKING_I> pi_ap("0x3.243F6A8885A308D3");
+  static const ap_fixed<CORDIC_INPUT_W + 2, CORDIC_WORKING_I> pi2_ap("0x1.921FB54442D1846");    // pi/2
+  static const ap_fixed<CORDIC_INPUT_W + 1, CORDIC_WORKING_I> pi4_ap("0x0.C90FDAA22168C23");    // pi/4
+  static const ap_fixed<CORDIC_INPUT_W + 1, CORDIC_WORKING_I> pi3n_ap("-0x2.5B2F8FE6643A469");  // -3*pi/4
+
+  // Useful typedefs for CORDIC implementation
+  typedef ap_fixed<CORDIC_INPUT_W + 1, CORDIC_INPUT_I + 1> cordic_abs_t;
+  typedef ap_fixed<CORDIC_INPUT_W + 1, 2> cordic_scaled_input_t;
+  typedef ap_fixed<CORDIC_WORKING_W, CORDIC_WORKING_I> cordic_working_t;
+
+  // The exact LUT values used by the HLS implementation
+  static const std::array<ap_ufixed<128, 2>, 23> atan_lut = {
+      "0x0.C90FDAA22168C234C4C6628B80DC1CD0", "0x0.76B19C1586ED3DA2B7F222F65E1D4680",
+      "0x0.3EB6EBF25901BAC55B71E7BD7DE885F8", "0x0.1FD5BA9AAC2F6DC65912F313E7D111DC",
+      "0x0.0FFAADDB967EF4E36CB2792DC0E2E0D4", "0x0.07FF556EEA5D892A13BCEBBB6ED46310",
+      "0x0.03FFEAAB776E5356EF9E31590057DD80", "0x0.01FFFD555BBBA972D00C46A3F77CC15C",
+      "0x0.00FFFFAAAADDDDB94BB12AFB6B6D4F7C", "0x0.007FFFF55556EEEEA5CA6ADEAB02251C",
+      "0x0.003FFFFEAAAAB77776E52E5A019FBCE8", "0x0.001FFFFFD55555BBBBBA972976256248",
+      "0x0.000FFFFFFAAAAAADDDDDDB94B94D5BD4", "0x0.0007FFFFFF5555556EEEEEEA5CA5CB40",
+      "0x0.0003FFFFFFEAAAAAAB7777776E52E52C", "0x0.0001FFFFFFFD5555555BBBBBBBA97294",
+      "0x0.0000FFFFFFFFAAAAAAAADDDDDDDDB948", "0x0.00007FFFFFFFF555555556EEEEEEEEA4",
+      "0x0.00003FFFFFFFFEAAAAAAAAB777777774", "0x0.00001FFFFFFFFFD555555555BBBBBBB8",
+      "0x0.00000FFFFFFFFFFAAAAAAAAAADDDDDDC", "0x0.000007FFFFFFFFFF55555555556EEEEC",
+      "0x0.000003FFFFFFFFFFEAAAAAAAAAAB7774"};
+
   // Class for intermediate variables
   class PtPxPy {
   public:
@@ -35,7 +71,7 @@ namespace P2L1HTMHTEmu {
     pxy_t px = 0.;
     pxy_t py = 0.;
 
-    PtPxPy operator+(const PtPxPy& b) const {
+    PtPxPy operator+(const PtPxPy &b) const {
       PtPxPy c;
       c.pt = this->pt + b.pt;
       c.px = this->px + b.px;
@@ -68,29 +104,6 @@ namespace P2L1HTMHTEmu {
   // Replicates the fixed-point CORDIC arithmetic bit-exactly so that the CMSSW
   // emulator matches the HLS firmware/csim output.
   inline ap_fixed<12, 3> atan2_cordic(pxy_t in1, pxy_t in2) {
-    static constexpr int W = pxy_t::width;
-    static constexpr int I = pxy_t::iwidth;
-    static constexpr int CORDIC_GUARD_BITS = 7;
-    static constexpr int WC = W + CORDIC_GUARD_BITS;  // Larger internal working bitwidth with "enough" guard bits
-    static constexpr int WCI = 3;                     // Number of integer bits in WC
-    static constexpr int NITER =
-        WC -
-        WCI;  // Number of iterations to perform, correspond to number of fractional bits in the working bitwidth representation
-
-    // Fixed constants, in precision used by the HLS implementation
-    static const ap_fixed<W + 1, 3> pi_ap = M_PI;
-    static const ap_fixed<WC, 3> pi2_ap = M_PI / 2.0;
-    static const ap_fixed<W + 1, 3> pi4_ap = M_PI / 4.0;
-    static const ap_fixed<W + 1, 3> pi3n_ap = -3.0 * M_PI / 4.0;
-
-    // LUT of atan(2^-i) for each iteration 0->NITER-1 in precision of working type WC
-    static const auto atan_lut = [] {
-      std::array<ap_fixed<WC, WCI>, NITER> lut{};
-      for (int i = 0; i < NITER; ++i)
-        lut[i] = std::atan(std::ldexp(1.0, -i));  // atan(2^-i)
-      return lut;
-    }();
-
     // Encode the sign of the inputs (0=negative, 1=zero, 2=positive)
     const ap_uint<2> signin1 = (in1 > 0) ? 2 : (in1 == 0) ? 1 : 0;
     const ap_uint<2> signin2 = (in2 > 0) ? 2 : (in2 == 0) ? 1 : 0;
@@ -116,37 +129,39 @@ namespace P2L1HTMHTEmu {
 
     // Absolute values of inputs
     // Widen by one bit to ensure -in1/2 is representable
-    ap_fixed<W + 1, I + 1> in1abs = (signin1 == 0) ? (ap_fixed<W + 1, I + 1>)(-in1) : (ap_fixed<W + 1, I + 1>)(in1);
-    ap_fixed<W + 1, I + 1> in2abs = (signin2 == 0) ? (ap_fixed<W + 1, I + 1>)(-in2) : (ap_fixed<W + 1, I + 1>)(in2);
+    cordic_abs_t in1abs = (signin1 == 0) ? cordic_abs_t(-in1) : cordic_abs_t(in1);
+    cordic_abs_t in2abs = (signin2 == 0) ? cordic_abs_t(-in2) : cordic_abs_t(in2);
 
     // Bit reinterpretation
     // CORDIC prefers working with ~2 integer bits and many fractional bits
-    ap_fixed<W + 1, 2> in1abs_sft, in2abs_sft;
+    cordic_scaled_input_t in1abs_sft, in2abs_sft;
     in1abs_sft.range() = in1abs.range();
     in2abs_sft.range() = in2abs.range();
 
     // Ensure cx >= cy for CORDIC, swap in2 and in1 if necessary
     // CORDIC then operates in 0-pi/4 range
-    const bool swap = (in1abs <= in2abs);
-    ap_fixed<WC, 3> cx = swap ? in2abs_sft : in1abs_sft;
-    ap_fixed<WC, 3> cy = swap ? in1abs_sft : in2abs_sft;
-    ap_fixed<WC, 3> cz = 0;  // Initial angle accumulator
+    const bool swap = (in1abs < in2abs);
+    cordic_working_t cx = swap ? in2abs_sft : in1abs_sft;
+    cordic_working_t cy = swap ? in1abs_sft : in2abs_sft;
+    cordic_working_t cz = 0;  // Initial angle accumulator
 
     // CORDIC iterations
     // Each iteration rotates the vector (cx, cy) by atan(2^-i) towards the x-axis, accumulating the angle in cz.
     // Sign check of cy determines the direction of rotation for the current iteration. i.e. if cy<0, the previous iteration overshot the x-axis and the next iteration rotates back towards the x-axis.
     // After all iterations, cy~0 and cz contains the angle of the original vector (in1, in2) in radians.
-    for (int i = 0; i < NITER; ++i) {
-      ap_fixed<WC, 3> cx_new, cy_new, cz_new;
+    for (int i = 0; i < CORDIC_NITER; ++i) {
+      cordic_working_t cx_new, cy_new, cz_new;
 
-      if (cy >= 0) {
+      cordic_working_t angle = cordic_working_t(atan_lut[i]);  // convert once
+
+      if (cy[CORDIC_WORKING_W - 1] == 0) {
         cx_new = cx + (cy >> i);
         cy_new = cy - (cx >> i);
-        cz_new = cz + atan_lut[i];
+        cz_new = cz + angle;
       } else {
         cx_new = cx - (cy >> i);
         cy_new = cy + (cx >> i);
-        cz_new = cz - atan_lut[i];
+        cz_new = cz - angle;
       }
 
       cx = cx_new;
@@ -170,7 +185,6 @@ namespace P2L1HTMHTEmu {
 
   inline etaphi_t phi_cordic(pxy_t y, pxy_t x) {
 #ifdef CMSSW_GIT_HASH
-    // ap_fixed<12, 3> phi = atan2(y.to_double(), x.to_double());  // hls_math.h not available yet in CMSSW
     ap_fixed<12, 3> phi = atan2_cordic(y, x);
 #else
     ap_fixed<12, 3> phi = hls::atan2(y, x);
@@ -217,7 +231,7 @@ inline l1ct::Sum htmht(std::vector<l1ct::Jet> jets) {
   std::vector<P2L1HTMHTEmu::PtPxPy> ptpxpy;
   ptpxpy.resize(jets.size());
   std::transform(
-      jets.begin(), jets.end(), ptpxpy.begin(), [](const l1ct::Jet& jet) { return P2L1HTMHTEmu::mht_compute(jet); });
+      jets.begin(), jets.end(), ptpxpy.begin(), [](const l1ct::Jet &jet) { return P2L1HTMHTEmu::mht_compute(jet); });
 
   // Sum pt, px, py over jets
   P2L1HTMHTEmu::PtPxPy hthxhy = std::accumulate(ptpxpy.begin(), ptpxpy.end(), P2L1HTMHTEmu::PtPxPy());
